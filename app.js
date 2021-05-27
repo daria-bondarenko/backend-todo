9dbc0837aed0c61198c65d25e21c225bce59e86b
daria-bondarenko <dbondarenko.exceedteam@gmail.com>
2021-05-27 16:47:28 +0300

    Improved function nesting

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
-const taskScheme = new Schema ({
+const taskScheme = new Schema({
   text: String,
   isCheck: Boolean
 });
@@ -19,31 +19,38 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.json());
 
-app.get('/allTasks', (req,res) => {
+app.get('/allTasks', (req, res) => {
   Task.find().then(result => {
     res.send({data: result})
   });
 });
 
-app.post('/createTask', (req,res) => {
-  console.log(req.body);
+app.post('/createTask', (req, res) => {
   const task = new Task(req.body);
   task.save().then(result => {
-    res.send('Task created');
+    Task.find().then(result => {
+      res.send({data: result});
+    });
+  });
+});
+
+app.patch('/editTask', (req, res) => {
+  const {_id, text, isCheck} = req.body;
+
+  Task.updateOne({_id},
+    {text, isCheck}).then(result => {
+    Task.find().then(result => {
+      res.send({data: result});
+    })
   })
 });
 
-app.patch('/editTask', (req,res) => {
-  console.log(req.body.id, req.body.text)
-  Task.updateOne({_id: req.body.id}, {text: req.body.text, isCheck: req.body.isCheck}).then(result => {
-    res.send('Task edited');
-  });
-});
-
-app.delete(`/deleteTask`, (req,res) => {
-  Task.deleteOne({_id: req.body.id}).then(result => {
-    res.send('Task deleted');
-  });
+app.delete(`/deleteTask`, (req, res) => {
+  Task.deleteOne({_id: req.body._id}).then(result => {
+    Task.find().then(result => {
+      res.send({data: result});
+    })
+  })
 });
 
 app.listen(4000, () => {
